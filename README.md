ff935f74a8d6a7576541d669cce18c769decae9f
Tugdual Grall <tugdual@gmail.com>
2015-01-04 14:15:35 +0100

    step-02: add ui/socket.io

--- README.md
+++ README.md
@@ -7,24 +7,26 @@ It uses :
 
  * MongoDB Capped Collections and Tailable Cursors
  * node.js
- 
- 
+
+
 ## Step 1 : Capped Collection and Tailable Cursors with node.js
+
+*See branch step-01*
 
 Installation:
 
-1- Create a new database 
-  
+1- Create a new database
+
   `` use chat ``  
 
 
 
-2- Create a Capped Collection 
+2- Create a Capped Collection
 
 `` db.createCollection('messages', { capped: true, size: 100000 }); ``
-  
-  
-  
+
+
+
 3- Insert a dummy document
 
 
@@ -42,9 +44,30 @@ Installation:
 
 6- Insert new documents in the collection
 
-`` db.messages.insert({"type":"message", "value":"New message"});``
+`` db.messages.insert({"type":"message", "text":"New message"});``
 
 You can see that message is capture by the node application
 
 
+## Step 2 : Adding User Interface with Socket.io
 
+*See branch step-02*
+
+In this step, the application has been improved to send message to an HTML client using [socket.io](http://socket.io).
+
+Once you hace checked out the step-02 branch and updated the dependencies `npm install` :
+
+1- Start the application
+
+`` node app``
+
+2- Open your browser and go to:
+
+[http://localhost:3000](http://localhost:3000)
+
+3- Insert new messages in the collection
+
+`` db.messages.insert({"type":"message", "text":"New message pushed in web client"});``
+
+
+ 
--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 "use strict";
 
-var mongo = require("mongodb");
+var mongo = require("mongodb"),
+    fs = require("fs"),         // to read static files
+    io = require("socket.io"),  // socket io server
+    http = require("http");
 
 var mongodbUri = "mongodb://127.0.0.1/chat";
+
+var app = http.createServer(handler);
+io = io.listen(app);
+app.listen(3000);
+console.log("http server on port 3000");
+
+
+function handler(req, res){
+  fs.readFile(__dirname + "/index.html",
+  function (err, data) {
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
 
 mongo.MongoClient.connect (mongodbUri, function (err, db) {
 
   db.collection('messages', function(err, collection) {
-    // open a tailable cursor
-    console.log("== open tailable cursor");
-    collection.find({}, {tailable:true, awaitdata:true, numberOfRetries:-1}).sort({ $natural: 1 }).each(function(err, doc) {
-      console.log(doc);
-    })
+
+    // open socket
+    io.sockets.on("connection", function (socket) {
+      // open a tailable cursor
+      console.log("== open tailable cursor");
+      collection.find({}, {tailable:true, awaitdata:true, numberOfRetries:-1}).sort({ $natural: 1 }).each(function(err, doc) {
+        console.log(doc);
+        // send message to client
+        if (doc.type == "message") {
+          socket.emit("message",doc);
+        }
+      })
+
+    });
+
   });
 
 });
